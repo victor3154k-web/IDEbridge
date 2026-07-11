6930ef6c7ccbd19f76db31e372e40c32679c9f60
Victor V. <victor3154k@gmail.com>
2026-07-11 14:29:53 -0300

    Improve error handling for file explorer commands
    Handle file explorer command errors more gracefully.

--- src-tauri/src/fs_extra.rs
+++ src-tauri/src/fs_extra.rs
@@ -2,21 +2,26 @@ use std::process::Command;
 
 #[tauri::command]
 pub async fn reveal_in_file_explorer(path: &str) -> Result<(), String> {
+    // Se estiver rodando no Android, apenas ignore o comando de abrir janela do PC e retorne sucesso
+    if cfg!(target_os = "android") {
+        return Ok(());
+    }
+
     if cfg!(target_os = "windows") {
         Command::new("explorer")
             .args(["/select,", &path.replace("/", "\\")]) // The comma after select is not a typo
             .spawn()
-            .expect("Failed to open file explorer");
+            .map_err(|e| e.to_string())?;
     } else if cfg!(target_os = "macos") {
         Command::new("open")
             .args(["-R", path])
             .spawn()
-            .expect("Failed to open finder");
+            .map_err(|e| e.to_string())?;
     } else {
         Command::new("xdg-open")
             .args([path])
             .spawn()
-            .expect("Failed to open file explorer");
+            .map_err(|e| e.to_string())?;
     }
 
     Ok(())
